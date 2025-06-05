7ad756e887e4abda23ae341635913888a81a4aa6
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-06-05 11:27:12 +0530

    Update vite.config.js

--- blog-app-frontend/vite.config.js
+++ blog-app-frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(
+  {
+    proxy: {
+      '/blogs': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        
+      },
+  },
+  plugins: [ tailwindcss(), react()],
+  }
+)
