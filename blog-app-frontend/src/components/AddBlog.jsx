7a92e2be5e411f8d8c600481fb9a2f0f4050fb7b
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-06-06 17:12:26 +0530

    Update AddBlog.jsx

--- blog-app-frontend/src/components/AddBlog.jsx
+++ blog-app-frontend/src/components/AddBlog.jsx
@@ -54,32 +54,32 @@ function AddBlog() {
 
     return (
         <>
-        <div>{pageTitle()} </div>
-        <div>
-
-            <form onSubmit={saveOrEdit}>
-                <div>
-                    <label>Title: </label>
+        <div className="w-full max-w-md">
+             <div className="text-center">{pageTitle()} </div>
+            <form onSubmit={saveOrEdit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="title">Title: </label>
                     <input 
                     type="text" 
                     name="title" 
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     value={title}
                     required
                     onChange={(e) => setTitle(e.target.value)}
                     />
                 </div>
-                    <div>
-                        <label>Description: </label>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="description">Description: </label>
                         <input 
                         type="text" 
-                        id="textbox"
                         name="description" 
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         value={description}
                         required
                         onChange={(e) => setDescription(e.target.value)}
                         />
                     </div>
-                <button>Submit</button>
+                <button class="bg-gray-500  text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
             </form>
 
         </div>
