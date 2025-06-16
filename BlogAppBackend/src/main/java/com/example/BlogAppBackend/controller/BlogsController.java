d263e8f3c61e25fe3972b427306eee36357d892e
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-06-16 19:44:44 +0530

    correct imports

--- BlogAppBackend/src/main/java/com/example/BlogAppBackend/controller/BlogsController.java
+++ BlogAppBackend/src/main/java/com/example/BlogAppBackend/controller/BlogsController.java
@@ -1,6 +1,12 @@
 //controller
 package com.example.BlogAppBackend.controller;
 
+import com.example.BlogAppBackend.model.Blogs;
+import com.example.BlogAppBackend.service.BlogsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
