17abe85fb8b027351374f4fe0e0e9e865bb53b50
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-06-16 19:43:57 +0530

    basics imports

--- BlogAppBackend/src/main/java/com/example/BlogAppBackend/controller/BlogsController.java
+++ BlogAppBackend/src/main/java/com/example/BlogAppBackend/controller/BlogsController.java
@@ -1,2 +1,11 @@
 //controller
 package com.example.BlogAppBackend.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/blogs")
+public class BlogsController {}
