31180fd7df4d63310c88b81eee6ef845a26ce0c1
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-06-16 19:46:34 +0530

    Update Blogs.java

--- BlogAppBackend/src/main/java/com/example/BlogAppBackend/model/Blogs.java
+++ BlogAppBackend/src/main/java/com/example/BlogAppBackend/model/Blogs.java
@@ -9,4 +9,46 @@ import java.util.Date;
 @Getter
 @Setter
 @Entity
-public class Blogs {}
+public class Blogs {
+   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Blogs{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
