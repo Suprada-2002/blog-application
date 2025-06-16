d1ce9559ddefc066dc7e804decf2a311bc3fb2d2
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-06-16 19:48:00 +0530

    Update BlogsService.java

--- BlogAppBackend/src/main/java/com/example/BlogAppBackend/service/BlogsService.java
+++ BlogAppBackend/src/main/java/com/example/BlogAppBackend/service/BlogsService.java
@@ -13,3 +13,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BlogsService {
+    @Autowired
+    private BlogsRepo blogsRepo;
+
+    public Optional<Blogs> getBlogById(Long id){
+        return blogsRepo.findById(id);
+    }
+
+    public List<Blogs> getAllBlogs(){
+        return blogsRepo.findAll();
+    }
+
+    public Blogs createABlog(Blogs blogs){
+        return blogsRepo.save(blogs);
+    }
+
+    public Blogs updateABlog(Long id,Blogs blogs){
+        Blogs exisitingBlog = blogsRepo.findById(id).orElseThrow(() -> new RuntimeException("Blog Not Found"));
+        exisitingBlog.setTitle(blogs.getTitle());
+        exisitingBlog.setDescription(blogs.getDescription());
+        return blogsRepo.save(exisitingBlog);
+    }
+
+    public ResponseEntity<String> deleteABlog(Long id){
+        Optional<Blogs> requestBlog = blogsRepo.findById(id);
+        if(requestBlog.isPresent()){
+            blogsRepo.delete(requestBlog.get());
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+//        blogsRepo.deleteById(id);
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+}
