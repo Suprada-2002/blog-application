b23f31784ead86b6803a2899fae0abe52537b5b9
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-06-16 19:47:10 +0530

    Update BlogsRepo.java

--- BlogAppBackend/src/main/java/com/example/BlogAppBackend/repo/BlogsRepo.java
+++ BlogAppBackend/src/main/java/com/example/BlogAppBackend/repo/BlogsRepo.java
@@ -1,2 +1,10 @@
 // repository
 package com.example.BlogAppBackend.repo;
+
+import com.example.BlogAppBackend.model.Blogs;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogsRepo extends JpaRepository<Blogs , Long> {
+}
