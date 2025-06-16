9e41e858c4da4076b2e610d00b38cd4b87b67324
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-06-16 19:45:12 +0530

    all routes

--- BlogAppBackend/src/main/java/com/example/BlogAppBackend/controller/BlogsController.java
+++ BlogAppBackend/src/main/java/com/example/BlogAppBackend/controller/BlogsController.java
@@ -14,4 +14,37 @@ import java.util.Optional;
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/blogs")
-public class BlogsController {}
+public class BlogsController {
+     @Autowired
+    private final BlogsService blogsService;
+
+    public BlogsController(BlogsService blogsService){
+        this.blogsService = blogsService;
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Blogs> getById(@PathVariable Long id){
+        return blogsService.getBlogById(id);
+    }
+    @GetMapping
+    public List<Blogs> getAll(){
+        return blogsService.getAllBlogs();
+    }
+
+    @PostMapping("/add")
+    public Blogs createBlog(@RequestBody @Validated Blogs blogs){
+        blogs.setCreatedAt(LocalDate.now());
+        System.out.println("Received blog: "+blogs);
+        return  blogsService.createABlog(blogs);
+    }
+
+    @PutMapping("/{id}")
+    public Blogs updateBlog(@PathVariable Long id,@RequestBody @Validated Blogs blogs){
+        return blogsService.updateABlog(id, blogs);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBlog(@PathVariable Long id){
+        blogsService.deleteABlog(id);
+    }
+}
