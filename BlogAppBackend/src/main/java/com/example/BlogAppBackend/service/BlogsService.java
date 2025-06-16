43801a364b76e9b43ee472687df0a1c3ab8ec6b1
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-06-16 19:47:44 +0530

    basic imports

--- BlogAppBackend/src/main/java/com/example/BlogAppBackend/service/BlogsService.java
+++ BlogAppBackend/src/main/java/com/example/BlogAppBackend/service/BlogsService.java
@@ -1,2 +1,15 @@
 // service
 package com.example.BlogAppBackend.service;
+
+import com.example.BlogAppBackend.model.Blogs;
+import com.example.BlogAppBackend.repo.BlogsRepo;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
